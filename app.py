260194325e0f609cbbe72e774f5b413395fdd146
Venkata Sai Suraj Chengalvala <55936921+schengal4@users.noreply.github.com>
2023-12-21 10:24:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,46 +10,55 @@ with st.form('risk_score_form'):
     predisposition_to_vasovagal_symptoms = st.radio(
         'Predisposition to Vasovagal Symptoms:',
         options=['No', 'Yes'],
-        index=0  # Default to 'No'
+        index=0,  # Default to 'No'
+        horizontal = True,
+        help = "Triggered by being in a warm crowded place, prolonged standing, fear, emotion, or pain"
     )
     history_of_heart_disease = st.radio(
         'History of Heart Disease:',
         options=['No', 'Yes'],
-        index=0  # Default to 'No'
+        index=0,  # Default to 'No'
+        horizontal = True,
+        help = "CAD, atrial fibrillation or flutter, CHF, valvular disease"
+        
     )
     systolic_bp_reading = st.radio(
         'Any Systolic Pressure Reading <90 or >180 mmHg:',
         options=['No', 'Yes'],
-        index=0  # Default to 'No'
+        index=0,  # Default to 'No'
+        horizontal = True
     )
 
     st.subheader('Investigations')
     elevated_troponin_level = st.radio(
         'Elevated Troponin Level:',
         options=['No', 'Yes'],
-        index=0  # Default to 'No'
+        index=0,  # Default to 'No'
+        horizontal = True
     )
     abnormal_qrs_axis = st.radio(
         'Abnormal QRS Axis:',
         options=['No', 'Yes'],
-        index=0  # Default to 'No'
+        index=0,  # Default to 'No'
+        horizontal = True
     )
     qrs_duration = st.radio(
         'QRS Duration >130 ms:',
         options=['No', 'Yes'],
-        index=0  # Default to 'No'
+        index=0,  # Default to 'No'
+        horizontal = True
     )
     corrected_qt_interval = st.radio(
         'Corrected QT Interval >480 ms:',
         options=['No', 'Yes'],
-        index=0  # Default to 'No'
+        index=0,  # Default to 'No'
+        horizontal = True
     )
-
-    st.subheader('Diagnosis in Emergency Department')
     diagnosis = st.radio(
-        'Diagnosis:',
+        'Emergency Department Diagnosis:',
         options=['Vasovagal Syncope', 'Cardiac Syncope', 'Neither'],
-        index=2  # Default to 'Neither'
+        index=2,  # Default to 'Neither'
+        horizontal = True
     )
 
     submit_button = st.form_submit_button('Calculate Risk Score')
