20e78c92f17b987a3f32ec5e53ae11d5155e1c4f
Venkata Sai Suraj Chengalvala <55936921+schengal4@users.noreply.github.com>
2023-12-21 09:50:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,79 @@
 import streamlit as st
 
-def calculate_risk_score(age, diastolic_bp, troponin, qrs_duration, shortness_of_breath, history_vasovagal, no_hist_cardiac_disease, abnormal_ecg):
+# Title and introduction
+st.title('Canadian Syncope Risk Score Calculator')
+st.write('This tool calculates the risk of serious adverse events after syncope.')
+
+# Creating a form for user input
+with st.form('risk_score_form'):
+    st.subheader('Clinical Evaluation')
+    predisposition_to_vasovagal_symptoms = st.selectbox(
+        'Predisposition to Vasovagal Symptoms:',
+        options=['No', 'Yes'],
+        format_func=lambda x: 'No' if x == 'No' else 'Yes, -1 Point'
+    )
+    history_of_heart_disease = st.checkbox('History of Heart Disease (+1 Point)')
+    systolic_bp_reading = st.number_input(
+        'Any Systolic Pressure Reading <90 or >180 mmHg (+2 Points)',
+        min_value=0, max_value=300, step=1
+    )
+
+    st.subheader('Investigations')
+    elevated_troponin_level = st.checkbox('Elevated Troponin Level (+2 Points)')
+    abnormal_qrs_axis = st.checkbox('Abnormal QRS Axis (+1 Point)')
+    qrs_duration = st.checkbox('QRS Duration >130 ms (+1 Point)')
+    corrected_qt_interval = st.checkbox('Corrected QT Interval >480 ms (+2 Points)')
+
+    st.subheader('Diagnosis in Emergency Department')
+    diagnosis = st.radio(
+        'Diagnosis:',
+        options=['Vasovagal Syncope', 'Cardiac Syncope', 'Neither'],
+        index=2,
+        format_func=lambda x: f'{x} ({"-2 Points" if x == "Vasovagal Syncope" else "+2 Points" if x == "Cardiac Syncope" else "0 Point"})'
+    )
+
+    submit_button = st.form_submit_button('Calculate Risk Score')
+
+if submit_button:
     score = 0
-
-    if age >= 50:
-        score += 1
-    if diastolic_bp <= 50:
-        score += 2
-    if troponin > 99:
-        score += 3
-    if qrs_duration > 130:
-        score += 2
-    if shortness_of_breath:
-        score += 1
-    if history_vasovagal:
+    # Clinical evaluation
+    if predisposition_to_vasovagal_symptoms == 'Yes':
         score -= 1
-    if no_hist_cardiac_disease:
-        score -= 1
-    if abnormal_ecg:
+    if history_of_heart_disease:
+        score += 1
+    if systolic_bp_reading < 90 or systolic_bp_reading > 180:
         score += 2
 
-    return score
+    # Investigations
+    if elevated_troponin_level:
+        score += 2
+    if abnormal_qrs_axis:
+        score += 1
+    if qrs_duration:
+        score += 1
+    if corrected_qt_interval:
+        score += 2
 
-st.title("Canadian Syncope Risk Score Calculator")
+    # Diagnosis in the emergency department
+    if diagnosis == 'Vasovagal Syncope':
+        score -= 2
+    elif diagnosis == 'Cardiac Syncope':
+        score += 2
 
-st.write("Please provide patient details:")
+    # Calculate risk category
+    risk_category = ''
+    if score <= -3:
+        risk_category = 'Very Low'
+    elif score <= 0:
+        risk_category = 'Low'
+    elif score <= 3:
+        risk_category = 'Medium'
+    elif score <= 6:
+        risk_category = 'High'
+    else:
+        risk_category = 'Very High'
 
-age = st.number_input("Age:", min_value=0, max_value=120)
-diastolic_bp = st.number_input("Diastolic Blood Pressure:", min_value=0, max_value=200)
-troponin = st.number_input("Troponin level:", min_value=0, max_value=5000)
-qrs_duration = st.number_input("QRS duration (ms):", min_value=0, max_value=500)
+    # Display the results
+    st.write(f'Your Canadian Syncope Risk Score is: {score}')
+    st.write(f'Estimated Risk Category: {risk_category}')
 
-shortness_of_breath = st.checkbox("Shortness of breath?")
-history_vasovagal = st.checkbox("History of vasovagal syncope?")
-no_hist_cardiac_disease = st.checkbox("No history of cardiac disease?")
-abnormal_ecg = st.checkbox("Abnormal ECG?")
-
-if st.button("Calculate Risk Score"):
-    score = calculate_risk_score(age, diastolic_bp, troponin, qrs_duration, shortness_of_breath, history_vasovagal, no_hist_cardiac_disease, abnormal_ecg)
-    st.write(f"Canadian Syncope Risk Score: **{score}**")
-
-st.write("Note: This app is for educational purposes only. Always consult with a medical professional before making clinical decisions.")
